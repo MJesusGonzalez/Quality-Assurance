d3dd30009046bc762985a872655e457c234d08d0
M. Jesus Gonzalez <marcelogonzalez1418@hotmail.com>
2024-02-18 10:51:29 -0500

    Api and Functional Test - POST Books

--- P3_Personal_Library/routes/api.js
+++ P3_Personal_Library/routes/api.js
@@ -1,47 +1,77 @@
 /*
-*
-*
-*       Complete the API routing below
-*       
-*       
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+"use strict";
 
+const mongoose = require("mongoose");
 module.exports = function (app) {
+  mongoose.connect(process.env.DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
 
-  app.route('/api/books')
-    .get(function (req, res){
+  const libraryModel = require("../models/lib");
+
+  app
+    .route("/api/books")
+    .get(function (req, res) {
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      libraryModel
+        .find({})
+        .exec()
+        .then((data) => {
+          if (data) {
+            res.json(data);
+          }
+        })
+        .catch((err) => console.log(err));
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       let title = req.body.title;
       //response will contain new book object including atleast _id and title
+      if (!title) {
+        return res.send("missing required field title");
+      }
+
+      new libraryModel({
+        title: title,
+      }).save((err, data) => {
+        if (err) {
+          return console.log(err);
+        } else {
+          res.json({ _id: data._id, title: data.title });
+        }
+      });
     })
-    
-    .delete(function(req, res){
+
+    .delete(function (req, res) {
       //if successful response will be 'complete delete successful'
     });
 
-
-
-  app.route('/api/books/:id')
-    .get(function (req, res){
+  app
+    .route("/api/books/:id")
+    .get(function (req, res) {
       let bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
     })
-    
-    .post(function(req, res){
+
+    .post(function (req, res) {
       let bookid = req.params.id;
       let comment = req.body.comment;
       //json res format same as .get
     })
-    
-    .delete(function(req, res){
+
+    .delete(function (req, res) {
       let bookid = req.params.id;
       //if successful response will be 'delete successful'
     });
-  
 };
