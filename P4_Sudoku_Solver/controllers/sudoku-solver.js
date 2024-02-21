0d80c164d9309381e8ec5a5121b9914131a9e740
M. Jesus Gonzalez <marcelogonzalez1418@hotmail.com>
2024-02-21 00:14:26 -0500

    Unit Test Complete

--- P4_Sudoku_Solver/controllers/sudoku-solver.js
+++ P4_Sudoku_Solver/controllers/sudoku-solver.js
@@ -1,24 +1,25 @@
 class SudokuSolver {
-
   validate(puzzleString) {
+    const regex = /^[1-9.]+$/;
+
+    if (!puzzleString) return false;
+
+    if (!regex.test(puzzleString) || puzzleString.length !== 81) return false;
+
+    return true;
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
+  checkRowPlacement(puzzleString, row, column, value) {}
 
-  }
+  checkColPlacement(puzzleString, row, column, value) {}
 
-  checkColPlacement(puzzleString, row, column, value) {
+  checkRegionPlacement(puzzleString, row, column, value) {}
 
-  }
+  solve(puzzleString) {}
 
-  checkRegionPlacement(puzzleString, row, column, value) {
-
-  }
-
-  solve(puzzleString) {
-    
+  letterToNumber(row) {
+    return row.toUpperCase().charCodeAt(0) - "A".charCodeAt(0) + 1;
   }
 }
 
 module.exports = SudokuSolver;
-
