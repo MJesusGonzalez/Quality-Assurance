17d41af038fb80c3a1d8da81f004fcc752aa4428
M. Jesus Gonzalez <marcelogonzalez1418@hotmail.com>
2024-02-23 20:15:06 -0500

    Ps - Unit Test Complete

--- P5_American_British_Translator/tests/1_unit-tests.js
+++ P5_American_British_Translator/tests/1_unit-tests.js
@@ -1,8 +1,270 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Translator = require('../components/translator.js');
+const translator = require("../components/translator.js");
 
-suite('Unit Tests', () => {
+suite("Unit Tests", () => {
+  suite("American to British", () => {
+    // Test 1
+    test("Translate Mangoes are my favorite fruit. to British English", (done) => {
+      assert.equal(
+        translator("Mangoes are my favorite fruit.", "american-to-british")[0],
+        "Mangoes are my favourite fruit."
+      );
+      done();
+    });
 
+    // Test 2
+    test("Translate I ate yogurt for breakfast. to British English", (done) => {
+      assert.equal(
+        translator("I ate yogurt for breakfast.", "american-to-british")[0],
+        "I ate yoghurt for breakfast."
+      );
+      done();
+    });
+
+    // Test 3
+    test("Translate We had a party at my friend's condo. to British English", (done) => {
+      assert.equal(
+        translator(
+          "We had a party at my friend's condo.",
+          "american-to-british"
+        )[0],
+        "We had a party at my friend's flat."
+      );
+      done();
+    });
+
+    // Test 4
+    test("Translate Can you toss this in the trashcan for me? to British English", (done) => {
+      assert.equal(
+        translator(
+          "Can you toss this in the trashcan for me?",
+          "american-to-british"
+        )[0],
+        "Can you toss this in the bin for me?"
+      );
+      done();
+    });
+
+    // Test 5
+    test("Translate The parking lot was full. to British English", (done) => {
+      assert.equal(
+        translator("The parking lot was full.", "american-to-british")[0],
+        "The car park was full."
+      );
+      done();
+    });
+
+    // Test 6
+    test("Translate Like a high tech Rube Goldberg machine. to British English", (done) => {
+      assert.equal(
+        translator(
+          "Like a high tech Rube Goldberg machine.",
+          "american-to-british"
+        )[0],
+        "Like a high tech Heath Robinson device."
+      );
+      done();
+    });
+
+    // Test 7
+    test("Translate To play hooky means to skip class or work. to British English", (done) => {
+      assert.equal(
+        translator(
+          "To play hooky means to skip class or work.",
+          "american-to-british"
+        )[0],
+        "To bunk off means to skip class or work."
+      );
+      done();
+    });
+
+    // Test 8
+    test("Translate No Mr. Bond, I expect you to die. to British English", (done) => {
+      assert.equal(
+        translator("Mr. Bond, I expect you to die.", "american-to-british")[0],
+        "Mr Bond, I expect you to die."
+      );
+      done();
+    });
+
+    // Test 9
+    test("Translate Dr. Grosh will see you now. to British English", (done) => {
+      assert.equal(
+        translator("Dr. Grosh will see you now.", "american-to-british")[0],
+        "Dr Grosh will see you now."
+      );
+      done();
+    });
+
+    // Test 10
+    test("Translate Lunch is at 12:15 today. to British English", (done) => {
+      assert.equal(
+        translator("Lunch is at 12:15 today.", "american-to-british")[0],
+        "Lunch is at 12.15 today."
+      );
+      done();
+    });
+  });
+
+  suite("British to American", () => {
+    // Test 1
+    test("Translate We watched the footie match for a while. to American English", (done) => {
+      assert.equal(
+        translator(
+          "We watched the footie match for a while.",
+          "british-to-american"
+        )[0],
+        "We watched the soccer match for a while."
+      );
+      done();
+    });
+
+    // Test 2
+    test("Translate Paracetamol takes up to an hour to work. to American English", (done) => {
+      assert.equal(
+        translator(
+          "Paracetamol takes up to an hour to work.",
+          "british-to-american"
+        )[0],
+        "Tylenol takes up to an hour to work."
+      );
+      done();
+    });
+
+    // Test 3
+    test("Translate First, caramelise the onions. to American English", (done) => {
+      assert.equal(
+        translator("First, caramelise the onions.", "british-to-american")[0],
+        "First, caramelize the onions."
+      );
+      done();
+    });
+
+    // Test 4
+    test("Translate I spent the bank holiday at the funfair. to American English", (done) => {
+      assert.equal(
+        translator(
+          "I spent the bank holiday at the funfair.",
+          "british-to-american"
+        )[0],
+        "I spent the public holiday at the carnival."
+      );
+      done();
+    });
+
+    // Test 5
+    test("Translate I had a bicky then went to the chippy. to American English", (done) => {
+      assert.equal(
+        translator(
+          "I had a bicky then went to the chippy.",
+          "british-to-american"
+        )[0],
+        "I had a cookie then went to the fish-and-chip shop."
+      );
+      done();
+    });
+
+    // Test 6
+    test("Translate I've just got bits and bobs in my bum bag. to American English", (done) => {
+      assert.equal(
+        translator(
+          "I've just got bits and bobs in my bum bag.",
+          "british-to-american"
+        )[0],
+        "I've just got odds and ends in my fanny pack."
+      );
+      done();
+    });
+
+    // Test 7
+    test("Translate The car boot sale at Boxted Airfield was called off. to American English", (done) => {
+      assert.equal(
+        translator(
+          "The car boot sale at Boxted Airfield was called off.",
+          "british-to-american"
+        )[0],
+        "The swap meet at Boxted Airfield was called off."
+      );
+      done();
+    });
+
+    // Test 8
+    test("Translate Have you met Mrs Kalyani? to American English", (done) => {
+      assert.equal(
+        translator("Have you met Mrs Kalyani?", "british-to-american")[0],
+        "Have you met Mrs. Kalyani?"
+      );
+      done();
+    });
+
+    // Test 9
+    test("Translate Prof Joyner of King's College, London. to American English", (done) => {
+      assert.equal(
+        translator(
+          "Prof Joyner of King's College, London.",
+          "british-to-american"
+        )[0],
+        "Prof. Joyner of King's College, London."
+      );
+      done();
+    });
+
+    // Test 10
+    test("Translate Tea time is usually around 4 or 4.30. to American English", (done) => {
+      assert.equal(
+        translator(
+          "Tea time is usually around 4 or 4.30.",
+          "british-to-american"
+        )[0],
+        "Tea time is usually around 4 or 4:30."
+      );
+      done();
+    });
+  });
+
+  suite("Highlight", () => {
+    // Test 1
+    test("Highlight translation in Mangoes are my favorite fruit.", (done) => {
+      assert.equal(
+        translator("Mangoes are my favorite fruit.", "american-to-british")[1],
+        'Mangoes are my <span class="highlight">favourite</span> fruit.'
+      );
+      done();
+    });
+
+    // Test 2
+    test("Highlight translation in I ate yogurt for breakfast.", (done) => {
+      assert.equal(
+        translator("I ate yogurt for breakfast.", "american-to-british")[1],
+        'I ate <span class="highlight">yoghurt</span> for breakfast.'
+      );
+      done();
+    });
+
+    // Test 3
+    test("Highlight translation in We watched the footie match for a while.", (done) => {
+      assert.equal(
+        translator(
+          "We watched the footie match for a while.",
+          "british-to-american"
+        )[1],
+        'We watched the <span class="highlight">soccer</span> match for a while.'
+      );
+      done();
+    });
+
+    // Test 4
+    test("Highlight translation in Paracetamol takes up to an hour to work.", (done) => {
+      assert.equal(
+        translator(
+          "Paracetamol takes up to an hour to work.",
+          "british-to-american"
+        )[1],
+        '<span class="highlight">Tylenol</span> takes up to an hour to work.'
+      );
+      done();
+    });
+  });
 });
